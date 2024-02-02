87efb9c502d923148da05566890b2a3b8b5b3755
Atsushi <atu729@icloud.com>
2024-02-02 21:54:29 +0900

    import test function

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 // import Counter from "./pages/stateUpdate/before";
 // import Counter from "./pages/state-update/after";
 // import ProductCard from "./pages/conditional-rendering/after";
+// import Memo from "./pages/example-useMemo/before";
+import Memo from "./pages/example-useMemo/after";
 import Post from "./pages/fetching-in-useEffect/before";
 
 function App() {
-  return <Post />;
+  return <Memo />;
 }
 
 export default App;
